9a2f484135372ea70682d4f95c4778daf375d37d
Edmarielle <edmariellenocedal.bernaldez@bicol-u.edu.ph>
2024-03-20 08:36:11 +0800

    personal information with js

--- personal_info.php
+++ personal_info.php
@@ -12,23 +12,7 @@ $_SESSION['user_type'] = 'V';
     <title>Personal Information</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="icons/bootstrap-icons.css">
-    <style>
-        /* NAV */
-        nav {
-            background-color: #283872;
-            width: 100%;
-            box-shadow: 0px 2px 5px black;
-        }
-
-        nav a {
-            text-decoration: none;
-            color: #E4E9FF;
-        }
-
-        nav a:hover {
-            color: #0F1636;
-        }
-    </style>
+    <link rel="stylesheet" href="hris_style.css">
 </head>
 
 <body style="background-color: #80A1F5">
@@ -48,12 +32,11 @@ $_SESSION['user_type'] = 'V';
                 <form action="pds.php" method="post">
                     <div class="container px-5">
 
-                        <!-- EMPLOYEE FULL NAME -->
+                        <!-- EMPLOYEE'S FULL NAME -->
                         <div class="row mt-3">
                             <div class="col mx-2">
                                 <label for="name_last">SURNAME</label><br>
                                 <input type="text" required name="name_last" id="name_last" class="form-control">
-
                             </div>
                             <div class="col mx-2">
                                 <label for="name_first">FIRST NAME</label><br>
@@ -62,8 +45,7 @@ $_SESSION['user_type'] = 'V';
                             <div class="col mx-2">
                                 <label for="name_middle">MIDDLE NAME</label><br>
                                 <div style="display: inline-block; width: 74%">
-                                    <input type="text" required name="name_middle" id="name_middle"
-                                        class="form-control">
+                                    <input type="text" required name="name_middle" id="name_middle" class="form-control">
                                 </div>
                                 <div class="form-check form-check-inline ms-1">
                                     <input class="form-check-input" type="checkbox" id="null_middle">
@@ -139,7 +121,7 @@ $_SESSION['user_type'] = 'V';
                             </div>
                         </div>
 
-                        <!-- EMPLOYEE ID -->
+                        <!-- EMPLOYEE'S ID'S -->
                         <div class="row mt-3">
                             <div class="col mx-2">
                                 <label for="gsis">GSIS ID NO.</label><br>
@@ -176,7 +158,7 @@ $_SESSION['user_type'] = 'V';
                             </div>
                         </div>
 
-                        <!-- EMPLOYEE CITIZENSHIP -->
+                        <!-- EMPLOYEE'S CITIZENSHIP -->
                         <div class="row mt-3">
                             <div class="col mx-2">
                                 <label for="citizenship">CITIZENSHIP</label><br>
@@ -422,128 +404,8 @@ $_SESSION['user_type'] = 'V';
             </div>
         </div>
 
-        <!-- JAVASCRIPT -->
-        <script>
+        <script type="text/javascript" src="personal_info_script.js"></script>
 
-            // ======================= N/A disable =======================
-            function setupNullInput(checkboxId, inputId) {
-                const checkbox = document.getElementById(checkboxId);
-                const input = document.getElementById(inputId);
-
-                checkbox.addEventListener('change', function () {
-                    if (this.checked) {
-                        input.value = "N/A";
-                        input.disabled = true;
-                    } else {
-                        input.value = "";
-                        input.disabled = false;
-                    }
-                });
-            }
-
-            // Call the function for each pair of checkbox and input
-            setupNullInput('null_middle', 'name_middle');
-            setupNullInput('null_ext', 'name_ext');
-            setupNullInput('null_rhbl', 'radd_houseblocklot');
-            setupNullInput('null_rst', 'radd_street');
-            setupNullInput('null_rsv', 'radd_subdivisionvillage');
-            setupNullInput('null_phbl', 'padd_houseblocklot');
-            setupNullInput('null_pst', 'padd_street');
-            setupNullInput('null_psv', 'padd_subdivisionvillage');
-            setupNullInput('null_telno', 'tel_no');
-            setupNullInput('null_emailadd', 'email_add');
-
-            // ================================= Citizenship =================================
-            const citizenshipSelect = document.getElementById('citizenship');
-            const citizenshipBySelect = document.getElementById('citizenship_by');
-            const citizenshipCountryInput = document.getElementById('citizenship_country');
-
-            citizenshipSelect.addEventListener('change', function () {
-                if (this.value === 'D') {
-                    // If Dual Citizenship is selected, enable Citizenship By and Citizenship Country
-                    citizenshipBySelect.disabled = false;
-                    citizenshipCountryInput.disabled = false;
-                } else {
-                    // Otherwise, disable them
-                    citizenshipBySelect.disabled = true;
-                    citizenshipCountryInput.disabled = true;
-                    // Reset Citizenship By and Citizenship Country values when Citizenship changes
-                    citizenshipBySelect.value = '';
-                    citizenshipCountryInput.value = '';
-                }
-            });
-
-            // ============ Same as the Residential Address Checkbox ============
-            function copyValues(source, destination, isChecked, checkbox) {
-                // destination.value = isChecked ? source.value : '';
-                if (chk_same.checked) {
-                    destination.value = source.value;
-                    destination.disabled = true;
-
-                    // If source is null, input is disabled and N/A checkbox is checked
-                    if (source.value == 'N/A') {
-                        checkbox.checked = true;
-                    }
-                } else {
-                    destination.value = '';
-                    destination.disabled = false;
-                    checkbox.checked = false;
-                }
-            }
-
-            // Get references to the checkbox and input text elements
-            const chk_same = document.getElementById('same_add');
-            const sourceInputs = [
-                document.getElementById('radd_houseblocklot'),
-                document.getElementById('radd_street'),
-                document.getElementById('radd_subdivisionvillage'),
-                document.getElementById('radd_barangay'),
-                document.getElementById('radd_citymunicipality'),
-                document.getElementById('radd_province'),
-                document.getElementById('radd_zipcode')
-            ];
-            const destinationInputs = [
-                document.getElementById('padd_houseblocklot'),
-                document.getElementById('padd_street'),
-                document.getElementById('padd_subdivisionvillage'),
-                document.getElementById('padd_barangay'),
-                document.getElementById('padd_citymunicipality'),
-                document.getElementById('padd_province'),
-                document.getElementById('padd_zipcode'),
-            ];
-            const naCheckboxes = [
-                document.getElementById('null_phbl'),
-                document.getElementById('null_pst'),
-                document.getElementById('null_psv')
-            ];
-
-            // Add event listener to the checkbox
-            chk_same.addEventListener('change', function () {
-                // Iterate over source and destination inputs, copying values accordingly
-                sourceInputs.forEach((source, index) => {
-                    copyValues(source, destinationInputs[index], chk_same.checked, naCheckboxes[index]);
-                });
-            });
-
-            // ===================================== CITY/MUNICIPALITY FILTER =====================================
-            $(document).ready(function () {
-                $('#radd_province').change(function () {
-                    var provinceName = $(this).val();
-                    if (provinceName) {
-                        $.ajax({
-                            type: 'POST',
-                            url: 'fetch_cities.php', // Provide the URL to the server-side script to handle the AJAX request
-                            data: { province_name: provinceName },
-                            success: function (html) {
-                                $('#radd_citymunicipality').html(html);
-                            }
-                        });
-                    } else {
-                        $('#radd_citymunicipality').html('<option value="" disabled selected value>--select--</option>');
-                    }
-                });
-            });
-        </script>
 </body>
 
 </html>
