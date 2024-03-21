829774ba0b77b45b0b204bb14aa06cdd37d6a1a3
Edmarielle <edmariellenocedal.bernaldez@bicol-u.edu.ph>
2024-03-21 15:01:29 +0800

    created pds form - incomplete (pi-fb only)

--- pds_form_script.js
+++ pds_form_script.js
@@ -1,32 +1,3 @@
-// ======================= N/A disable =======================
-function setupNullInput(checkboxId, inputId) {
-  const checkbox = document.getElementById(checkboxId);
-  const input = document.getElementById(inputId);
-
-  checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      input.value = "N/A";
-      input.disabled = true;
-    } else {
-      input.value = "";
-      input.disabled = false;
-    }
-  });
-}
-
-// Call the function for each pair of checkbox and input
-// PERSONAL INFORMATION
-setupNullInput("null_middle", "name_middle");
-setupNullInput("null_ext", "name_ext");
-setupNullInput("null_rhbl", "radd_houseblocklot");
-setupNullInput("null_rst", "radd_street");
-setupNullInput("null_rsv", "radd_subdivisionvillage");
-setupNullInput("null_phbl", "padd_houseblocklot");
-setupNullInput("null_pst", "padd_street");
-setupNullInput("null_psv", "padd_subdivisionvillage");
-setupNullInput("null_telno", "tel_no");
-setupNullInput("null_emailadd", "email_add");
-
 // ================================= Citizenship =================================
 const citizenshipSelect = document.getElementById("citizenship");
 const citizenshipBySelect = document.getElementById("citizenship_by");
@@ -101,6 +72,113 @@ chk_same.addEventListener("change", function () {
     );
   });
 });
+
+// ======================= N/A disable =======================
+function setupNullInput(checkboxId, inputId) {
+  const checkbox = document.getElementById(checkboxId);
+  const input = document.getElementById(inputId);
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      input.value = "N/A";
+      input.disabled = true;
+    } else {
+      input.value = "";
+      input.disabled = false;
+    }
+  });
+}
+
+// Call the function for each pair of checkbox and input
+// PERSONAL INFORMATION
+setupNullInput("null_middle", "name_middle");
+setupNullInput("null_ext", "name_ext");
+setupNullInput("null_rhbl", "radd_houseblocklot");
+setupNullInput("null_rst", "radd_street");
+setupNullInput("null_rsv", "radd_subdivisionvillage");
+setupNullInput("null_phbl", "padd_houseblocklot");
+setupNullInput("null_pst", "padd_street");
+setupNullInput("null_psv", "padd_subdivisionvillage");
+setupNullInput("null_telno", "tel_no");
+setupNullInput("null_emailadd", "email_add");
+// FAMILY BACKGROUND
+setupNullInput("null_spouse_mi", "spousename_middle");
+setupNullInput("null_spouse_nameext", "spousename_extension");
+setupNullInput("null_occupation", "occupation");
+setupNullInput("null_bus", "business_name");
+setupNullInput("null_busadd", "business_address");
+setupNullInput("null_spouse_telno", "spouse_telno");
+setupNullInput("null_father_mi", "fathername_middle");
+setupNullInput("null_father_nameext", "fathername_extension");
+setupNullInput("null_mother_mi", "mothername_middle");
+
+// ============================ N/A DISABLE ARRAY ============================
+function setupNullInputArray(checkboxId, inputIds, chkboxIds) {
+  const checkbox = document.getElementById(checkboxId);
+  const inputs = inputIds.map((id) => document.getElementById(id));
+  const checkboxes = chkboxIds.map((id) => document.getElementById(id));
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      inputs.forEach((input) => {
+        input.value = "N/A";
+        input.disabled = true;
+      });
+      checkboxes.forEach((chkbx) => {
+        chkbx.checked = true;
+        chkbx.disabled = true;
+      });
+    } else {
+      inputs.forEach((input) => {
+        input.value = "";
+        input.disabled = false;
+      });
+      checkboxes.forEach((chkbx) => {
+        chkbx.checked = false;
+        chkbx.disabled = false;
+      });
+    }
+  });
+}
+
+// ARRAYS
+setupNullInputArray(
+  "null_spouse",
+  [
+    "spousename_last",
+    "spousename_first",
+    "spousename_middle",
+    "spousename_extension",
+    "occupation",
+    "business_name",
+    "business_address",
+    "spouse_telno",
+  ],
+  [
+    "null_spouse_mi",
+    "null_spouse_nameext",
+    "null_occupation",
+    "null_bus",
+    "null_busadd",
+    "null_spouse_telno",
+  ]
+);
+setupNullInputArray(
+  "null_father",
+  [
+    "fathername_last",
+    "fathername_first",
+    "fathername_middle",
+    "fathername_extension",
+  ],
+  ["null_father_mi", "null_father_nameext"]
+);
+setupNullInputArray(
+  "null_mother",
+  ["mothername_last", "mothername_first", "mothername_middle"],
+  ["null_mother_mi"]
+);
+setupNullInputArray("null_children", ["child_name", "child_dob"], []);
 
 // // ===================================== CITY/MUNICIPALITY FILTER =====================================
 // $(document).ready(function () {
--- topnav.php
+++ topnav.php
@@ -1,14 +1,14 @@
 <nav class="navbar sticky-top">
     <div class="container">
         <div class="col mx-4">
-            <a href="personal_info.php">
+            <a href="pds_form.php?form_section=personal_info">
                 <p style="text-align:center">
                     Personal Information
                 </p>
             </a>
         </div>
         <div class="col mx-4">
-            <a href="fam_bg.php">
+            <a href="pds_form.php?form_section=fam_bg">
                 <p style="text-align:center">
                     Family Background
                 </p>
@@ -25,6 +25,13 @@
             <a href="#">
                 <p style="text-align:center">
                     Civil Service Eligibility
+                </p>
+            </a>
+        </div>
+        <div class="col mx-4">
+            <a href="#">
+                <p style="text-align:center">
+                    Work Experience
                 </p>
             </a>
         </div>
